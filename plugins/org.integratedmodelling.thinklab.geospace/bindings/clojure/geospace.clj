316ee67255f233cd7be38d23af08beb85f6c91c9
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2008-11-15 21:32:49 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/thinklab@1791 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.thinklab.geospace/bindings/clojure/geospace.clj
@@ -0,0 +1,24 @@
+;; -----------------------------------------------------------------------------------------
+;; Clojure bindings for the Geospace plugin
+;; VERY incomplete, stubs only
+;; @author Ferdinando Villa
+;; @date Nov 15, 2008
+;; -----------------------------------------------------------------------------------------
+
+(in-ns 'tl)
+
+(defn get-boundary
+	"Returns a space areal observation with the boundary of the given observation, or the convex hull
+	 of a list of observations."
+	 []
+	 nil)
+	 
+(defn grid
+	"Returns a new grid observation with the given attributes"
+	[]
+	nil)
+	
+(defn areal-location
+		"Returns a new areal space observation with the given shape"
+	[shape]
+	nil)
