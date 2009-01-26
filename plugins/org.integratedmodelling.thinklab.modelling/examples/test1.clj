590d9f3f6dca7560297393426eadc5d893305517
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-01-26 22:52:45 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/thinklab@1888 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.thinklab.modelling/examples/test1.clj
+++ plugins/org.integratedmodelling.thinklab.modelling/examples/test1.clj
@@ -6,16 +6,26 @@
 (load-bindings 'geospace)
 (load-bindings 'modelling)
 
-(model/make-model climateStabilityModel 
-    [:elevation (model/make-type habitat:Elevation (unit "m")) :population habitat:PopulationDensity]
+(let [
+	dElevation (model/make-type 
+				(model/make-type 'habitat:Elevation (corescience/make-unit "m"))
+				(corescience/make-discretizer 
+					"elevation:Low<-200)" 
+					"elevation:Medium<-[200,650)" 
+					"elevation:High<-[650" ))
+     ]
+  (model/make-model climateStabilityModel 
+      [:elevation dElevation :population habitat:PopulationDensity]
 	(carbonService:ClimateStability
-		((and (between :elevation 12.22 21.22) (between (get-longitude (get-space :context)) 30.22 34.84)) 
-		  carbonService:Discrete6CarbonSequestered)
-		((and (in-region (get-space :context) (get-region 'Madagascar)) (> :population 250)) 
-		  carbonService:TropicalCarbonSequestered)
-		(:default carbonService:GeneralCarbonSequestered))
+		(and (between :elevation 12.22 21.22) (between (get-longitude (get-space :context)) 30.22 34.84)) 
+		'carbonService:Discrete6CarbonSequestered
+		(and (in-region (get-space :context) (get-region 'Madagascar)) (> :population 250)) 
+		'carbonService:TropicalCarbonSequestered
+		:default carbonService:GeneralCarbonSequestered)
 	(habitat:RainfallAmount
-	    ((in-region (get-space :context) 'Tropics) carbonService:Discrete12RainfallPerMonth)
-	    (:default carbonService:GeneralRainfallPerYear)))
+	    (in-region (get-space :context) 'Tropics) 
+          'carbonService:Discrete12RainfallPerMonth
+	    :default 
+	    'carbonService:GeneralRainfallPerYear)))
 
  
