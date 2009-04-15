b27f5474a2a67217e380ea84c5bf26cbaace7fef
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-04-15 20:50:52 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/thinklab@2134 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.thinklab.corescience/src/org/integratedmodelling/corescience/literals/GeneralClassifier.java
+++ plugins/org.integratedmodelling.thinklab.corescience/src/org/integratedmodelling/corescience/literals/GeneralClassifier.java
@@ -1,28 +1,42 @@
 package org.integratedmodelling.corescience.literals;
 
+import java.util.HashSet;
+
 import org.integratedmodelling.thinklab.interfaces.knowledge.IConcept;
+import org.integratedmodelling.thinklab.literals.AlgorithmValue;
+import org.integratedmodelling.thinklab.literals.IntervalValue;
 
 public class GeneralClassifier {
 
 	GeneralClassifier[] classifiers = null;
 	
-	public IConcept classify(Object o) {
-		
-		IConcept ret = null;
+	IntervalValue interval = null;
+	HashSet<IConcept> concepts = null;
+	AlgorithmValue code = null;
+	IConcept target = null;
+	
+	public boolean classify(Object o) {
 		
 		if (classifiers != null) {
 			for (GeneralClassifier cl : classifiers) {
-				if ( (ret = cl.classify(o)) != null)
-					break;
+				if (cl.classify(o))
+					return true;
 			}
-		} else {
+		} else if (interval != null) {
 			
-			/*
-			 * 
-			 */
+		} else if (concepts != null) {
+			
+		} else if (code != null) {
+			
 		}
 		
-		return ret;
+		return false;
 	}
+
+	public void setClass(IConcept c) {
+		target = c;
+	}
+	
+
 	
 }
--- plugins/org.integratedmodelling.thinklab.modelling/src/org/integratedmodelling/modelling/corescience/ClassificationModel.java
+++ plugins/org.integratedmodelling.thinklab.modelling/src/org/integratedmodelling/modelling/corescience/ClassificationModel.java
@@ -1,7 +1,9 @@
 package org.integratedmodelling.modelling.corescience;
 
+import org.integratedmodelling.corescience.literals.GeneralClassifier;
 import org.integratedmodelling.modelling.DefaultStatefulAbstractModel;
 import org.integratedmodelling.modelling.interfaces.IModel;
+import org.integratedmodelling.thinklab.KnowledgeManager;
 import org.integratedmodelling.thinklab.exception.ThinklabException;
 import org.integratedmodelling.thinklab.exception.ThinklabValidationException;
 import org.integratedmodelling.thinklab.interfaces.applications.ISession;
@@ -9,6 +11,10 @@ import org.integratedmodelling.thinklab.interfaces.knowledge.IConcept;
 import org.integratedmodelling.thinklab.interfaces.knowledge.IInstance;
 import org.integratedmodelling.thinklab.interfaces.storage.IKBox;
 import org.integratedmodelling.utils.Polylist;
+
+import clojure.lang.IPersistentSet;
+import clojure.lang.IPersistentVector;
+import clojure.lang.ISeq;
 
 public class ClassificationModel extends DefaultStatefulAbstractModel {
 
@@ -24,9 +30,9 @@ public class ClassificationModel extends DefaultStatefulAbstractModel {
 		}
 	}
 
-	public void addClassifier(Object classifier, Object concept) {
+	public GeneralClassifier getClassifier(Object classifier) {
 
-		System.out.println("got classifier " + classifier.getClass() + ": " + classifier + " for " + concept);
+		GeneralClassifier ret = null;
 		
 		/*
 		 * classifier can be:
@@ -37,6 +43,53 @@ public class ClassificationModel extends DefaultStatefulAbstractModel {
 		 *  List    (executable code, run after setting self to state)
 		 *  Set     (set of values to choose from: final match is an OR on the contents)
 		 */
+		if (classifier instanceof Integer || classifier instanceof Integer) {
+			
+			/*
+			 * match values
+			 */
+			
+		} else if (classifier instanceof IPersistentVector) {
+			
+			/*
+			 * match ranges
+			 */
+			
+		} else if (classifier instanceof IPersistentSet) {
+			
+			/*
+			 * make one classifier per element
+			 */
+			
+		} else if (classifier instanceof ISeq) {
+			
+			/*
+			 * code to be execd
+			 */
+			
+		} else {
+			
+			/*
+			 * convert to string and see if it's a concept
+			 */
+		}
+		
+		return ret;
+
+		
+	}
+	
+	public void addClassifier(Object classifier, Object concept) throws ThinklabException {
+
+		System.out.println("got classifier " + classifier.getClass() + ": " + classifier + " for " + concept);
+		
+		GeneralClassifier cl = getClassifier(classifier);
+		IConcept c = 
+			concept instanceof IConcept ? 
+					(IConcept)concept : 
+						KnowledgeManager.get().requireConcept(concept.toString());
+					
+		cl.setClass(c);
 		
 	}
 	
