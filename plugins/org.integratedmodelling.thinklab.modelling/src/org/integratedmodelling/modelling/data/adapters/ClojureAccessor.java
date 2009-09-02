7b37ce581c48066dee027e08221184a165e83e0a
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-09-02 01:30:41 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/thinklab@2323 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.thinklab.modelling/src/org/integratedmodelling/modelling/data/adapters/ClojureAccessor.java
+++ plugins/org.integratedmodelling.thinklab.modelling/src/org/integratedmodelling/modelling/data/adapters/ClojureAccessor.java
@@ -8,6 +8,15 @@ public class ClojureAccessor {
 	
 	public void initialize() {
 		
+		/*
+		 * figure out order of parameters
+		 */
+
+		/*
+		 * build and eval Clojure code, returning a proxy for this calculation;
+		 * put the proxy object in a safe place
+		 */
+	
 	}
 	
 	
