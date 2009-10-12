e85b3eebf99556af294aa33c354401e0e8c0a974
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2009-10-12 13:32:57 +0000

    Last fixes
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/thinklab@2368 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.thinklab.modelling/src/org/integratedmodelling/modelling/interfaces/IDataset.java
@@ -0,0 +1,43 @@
+package org.integratedmodelling.modelling.interfaces;
+
+import java.util.Collection;
+
+import org.integratedmodelling.corescience.interfaces.data.IContextualizedState;
+import org.integratedmodelling.thinklab.interfaces.knowledge.IConcept;
+
+/**
+ * An adapter that helps use and visualize an observation as a dataset with many
+ * possible data and uniform context. Should be initializable from a "live" observation or from any kind of persistent 
+ * storage. We may also want a factory that gives us a IDataset for any kind of
+ * suitable input.
+ * 
+ * @author Ferdinando Villa
+ *
+ */
+public interface IDataset {
+
+	public abstract boolean isSpatial();
+	
+	public abstract boolean isTemporal();
+	
+	public abstract int getStateCount();
+	
+	public abstract Collection<IConcept> getObservables();
+	
+	public abstract Collection<IConcept> getStatefulObservables();
+	
+	public abstract IContextualizedState getState(IConcept observable);
+	
+	public abstract String makeContourPlot(
+			IConcept observable, String fileOrNull, int x, int y, int ... flags);
+	
+	public abstract String makeSurfacePlot(
+			IConcept observable, String fileOrNull, int x, int y, int ... flags);
+
+	public abstract String makeTimeSeriesPlot(
+			IConcept observable, String fileOrNull, int x, int y, int ... flags);
+
+	public abstract String makeHistogramPlot(
+			IConcept observable, String fileOrNull, int x, int y, int ... flags);
+
+}
