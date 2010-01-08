b306c4a57f783dc957877ca28d0e64c09803f65a
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-01-08 03:43:16 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/thinklab@2551 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.thinklab.modelling/src/org/integratedmodelling/modelling/commands/CoverageCommand.java
@@ -0,0 +1,49 @@
+package org.integratedmodelling.modelling.commands;
+
+import java.util.HashMap;
+
+import org.integratedmodelling.corescience.interfaces.IObservationContext;
+import org.integratedmodelling.corescience.interfaces.IState;
+import org.integratedmodelling.modelling.Model;
+import org.integratedmodelling.modelling.ModelFactory;
+import org.integratedmodelling.thinklab.command.Command;
+import org.integratedmodelling.thinklab.exception.ThinklabException;
+import org.integratedmodelling.thinklab.interfaces.annotations.ThinklabCommand;
+import org.integratedmodelling.thinklab.interfaces.applications.ISession;
+import org.integratedmodelling.thinklab.interfaces.commands.ICommandHandler;
+import org.integratedmodelling.thinklab.interfaces.knowledge.IConcept;
+import org.integratedmodelling.thinklab.interfaces.literals.IValue;
+import org.integratedmodelling.thinklab.interfaces.storage.IKBox;
+import org.integratedmodelling.thinklab.kbox.KBoxManager;
+
+@ThinklabCommand(
+		name="coverage",
+		description="compute the extent coverage of a model in the linked kboxes",
+		argumentNames="model",
+		argumentTypes="thinklab-core:Text",
+		argumentDescriptions="the model id",
+		optionLongNames="kbox",
+		optionNames="k",
+		optionArgumentLabels="all kboxes",
+		optionTypes="thinklab-core:Text",
+		optionDescriptions="kbox"
+)
+public class CoverageCommand implements ICommandHandler {
+
+	IObservationContext ctx = null;
+	HashMap<IConcept, IState> states = new HashMap<IConcept, IState>();
+		@Override
+	public IValue execute(Command command, ISession session)
+			throws ThinklabException {
+		
+		String concept = command.getArgumentAsString("model");
+		
+		IKBox kbox = KBoxManager.get();
+		if (command.hasOption("kbox"))
+			kbox = KBoxManager.get().requireGlobalKBox(command.getOptionAsString("kbox"));
+		
+		Model model = ModelFactory.get().requireModel(concept);	
+		return ModelFactory.get().getSpatialCoverage(model, kbox, session);
+	}
+
+}
