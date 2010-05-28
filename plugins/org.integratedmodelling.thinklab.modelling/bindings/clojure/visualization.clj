6781dcfdfaa09e26f7f5c4a2258470abc9ab0fbb
sp4rt4n <sp4rt4n@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-05-28 18:30:37 +0000

    Removing obsolete clojure files.  These have been merged into modelling.clj.
    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/thinklab@2990 ca20354d-2f28-0410-8711-9259e250f5a2

--- plugins/org.integratedmodelling.thinklab.modelling/bindings/clojure/visualization.clj
+++
@@ -1,9 +0,0 @@
-(ns modelling)
-
-(defn write-netcdf
-	"Write out a netcdf file with all the states linked to the passed observation"
-	[observation filename]
-	(doto (new org.integratedmodelling.modelling.visualization.NetCDFArchive)
-		(.setObservation observation)
-		(.write filename)))
-		
