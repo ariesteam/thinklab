30ba9141ef4fdfc6ad27e65e397e141c4119c098
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-11-27 22:07:53 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/thinklab@3340 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.thinklab.modelling/src/org/integratedmodelling/modelling/visualization/presentation/Presentation.java
@@ -0,0 +1,150 @@
+package org.integratedmodelling.modelling.visualization.presentation;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.integratedmodelling.thinklab.exception.ThinklabException;
+import org.integratedmodelling.utils.xml.XMLDocument;
+import org.w3c.dom.Node;
+
+/**
+ * A descriptor for a "storyline". Serializable to/from XML. It's for now limited to one
+ * concept per page and associated (optional) visuals. Provides sequence and basic 
+ * content, no layout for now.
+ * 
+ * Sequence and off-sequence pages can be defined (to be interpreted by the view).
+ * 
+ * @author ferdinando.villa
+ *
+ */
+public class Presentation {
+
+	ArrayList<Page> pages = new ArrayList<Presentation.Page>();
+	ArrayList<Page> singlePages = new ArrayList<Presentation.Page>();
+	private String title;
+	private String description;
+	private String runningHead;
+	private String concept;
+	private String style;
+	
+	public class Page {
+
+		public String concept;
+		public String title;
+		public String description;
+		public String runningHead;
+		public String background;
+		public String name;
+		public String id;
+		public int sequence = -1;
+		public String getConcept() {
+			return concept;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public String getRunningHead() {
+			return runningHead;
+		}
+		public String getBackground() {
+			return background;
+		}
+		public String getName() {
+			return name;
+		}
+		public String getId() {
+			return id;
+		}
+		public int getSequence() {
+			return sequence;
+		}
+	}
+	
+	public void read(URL input) throws ThinklabException {
+		
+		XMLDocument doc = new XMLDocument(input);
+		
+		for (XMLDocument.NodeIterator it = doc.iterator(); it.hasNext(); ) {
+			Node node = it.next();
+			if (node.getNodeName().equals("page")) {
+				addPage(doc, node);
+			} else if (node.getNodeName().equals("title")) {
+				this.title = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("description")) {
+				this.description = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("runninghead")) {
+				this.runningHead = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("concept")) {
+				this.concept = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("style")) {
+				this.style = XMLDocument.getNodeValue(node);
+			}
+		}
+		
+	}
+
+	private void addPage(XMLDocument doc, Node root) {
+
+		Page page = new Page();
+		for (XMLDocument.NodeIterator ct = doc.iterator(root); ct.hasNext(); ) {
+			Node node = ct.next();
+			if (node.getNodeName().equals("type")) {
+			} else if (node.getNodeName().equals("title")) {
+				page.title = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("description")) {
+				page.description = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("runninghead")) {
+				page.runningHead = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("concept")) {
+				page.concept = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("background")) {
+				page.background = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("name")) {
+				page.name = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("id")) {
+				page.id = XMLDocument.getNodeValue(node);
+			} else if (node.getNodeName().equals("sequence")) {
+				page.sequence = Integer.parseInt(XMLDocument.getNodeValue(node));
+			}
+		}
+		if (page.sequence < 0) {
+			singlePages.add(page);
+		} else {
+			pages.ensureCapacity(page.sequence+1);
+			pages.add(page.sequence, page);
+		}
+	}
+
+	public ArrayList<Page> getPages() {
+		return pages;
+	}
+
+	public ArrayList<Page> getSinglePages() {
+		return singlePages;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getRunningHead() {
+		return runningHead;
+	}
+
+	public String getConcept() {
+		return concept;
+	}
+
+	public String getStyle() {
+		return style;
+	}
+	
+	
+}
---
+++ plugins/org.integratedmodelling.thinklab.modelling/src/org/integratedmodelling/modelling/visualization/presentation/PresentationFactory.java
@@ -0,0 +1,34 @@
+package org.integratedmodelling.modelling.visualization.presentation;
+
+import java.io.File;
+
+import org.integratedmodelling.thinklab.KnowledgeManager;
+import org.integratedmodelling.thinklab.exception.ThinklabException;
+import org.integratedmodelling.thinklab.interfaces.knowledge.IConcept;
+import org.integratedmodelling.thinklab.interfaces.knowledge.datastructures.IntelligentMap;
+
+public class PresentationFactory {
+
+	IntelligentMap<Presentation> _presentations;
+	
+	public Presentation getPresentation(IConcept concept) {
+		return _presentations.get(concept);
+	}
+	
+	public void scanDirectory(File dir) throws ThinklabException {
+		
+		for (File f : dir.listFiles()) {
+			if (f.toString().endsWith(".xml")) {
+				Presentation p = new Presentation();
+				try {
+					p.read(f.toURI().toURL());
+				} catch (Exception e) {
+					p = null;
+				}
+				if (p != null)
+					_presentations.put(KnowledgeManager.get().requireConcept(p.getConcept()), p);
+			}
+				
+		}
+	}
+}
