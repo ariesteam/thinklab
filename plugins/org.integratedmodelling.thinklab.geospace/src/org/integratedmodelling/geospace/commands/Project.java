d9b23fa345abb24906631a679e6946072973433b
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-12-04 20:06:50 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/thinklab@3370 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ plugins/org.integratedmodelling.thinklab.geospace/src/org/integratedmodelling/geospace/commands/Project.java
@@ -0,0 +1,48 @@
+package org.integratedmodelling.geospace.commands;
+
+import org.geotools.geometry.jts.JTS;
+import org.geotools.referencing.CRS;
+import org.integratedmodelling.geospace.Geospace;
+import org.integratedmodelling.thinklab.command.Command;
+import org.integratedmodelling.thinklab.exception.ThinklabException;
+import org.integratedmodelling.thinklab.interfaces.annotations.ThinklabCommand;
+import org.integratedmodelling.thinklab.interfaces.applications.ISession;
+import org.integratedmodelling.thinklab.interfaces.commands.ICommandHandler;
+import org.integratedmodelling.thinklab.interfaces.literals.IValue;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+@ThinklabCommand(
+		name="project",
+		description="project a point from a coordinate system to another",
+		argumentNames="source-srid,destination-srid,x-coordinate,y-coordinate",
+		argumentDescriptions="source EPSG code,destination EPSG code, X coordinate, Y coordinate",
+		argumentTypes="thinklab-core:Text,thinklab-core:Text,thinklab-core:Text,thinklab-core:Text,")
+public class Project implements ICommandHandler {
+
+	@Override
+	public IValue execute(Command command, ISession session)
+			throws ThinklabException {
+
+		String ssrid = command.getArgumentAsString("source-srid");
+		String dsrid = command.getArgumentAsString("destination-srid");
+		double xcoor = Double.parseDouble(command.getArgumentAsString("x-coordinate"));
+		double ycoor = Double.parseDouble(command.getArgumentAsString("y-coordinate"));
+		
+		CoordinateReferenceSystem sr = Geospace.getCRSFromID("EPSG:" + ssrid);
+		CoordinateReferenceSystem dr = Geospace.getCRSFromID("EPSG:" + dsrid);
+		
+		Coordinate nc = null; 
+		try {
+			nc = JTS.transform(new Coordinate(xcoor, ycoor), null, CRS.findMathTransform(sr, dr));
+		} catch (Exception e) {
+			throw new ThinklabException(e);
+		}
+		
+		session.getOutputStream().println("" + nc.x + " " + nc.y);
+		
+		return null;
+	}
+
+}
