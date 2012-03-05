b8c24b02f794daabcc0fb5dbf61044d9b0021302
Ferdinando Villa <ferdinando.villa@gmail.com>
2012-03-05 09:00:31 +0100

    Stubs for datasource functions

---
+++ plugins/org.integratedmodelling.thinklab.core/src/org/integratedmodelling/thinklab/functions/geospace/WFS.java
@@ -0,0 +1,23 @@
+package org.integratedmodelling.thinklab.functions.geospace;
+
+import java.util.Map;
+
+import org.integratedmodelling.thinklab.api.knowledge.IExpression;
+import org.integratedmodelling.thinklab.interfaces.annotations.Function;
+
+@Function(id="wfs", parameterNames= { "service", "id" })
+public class WFS implements IExpression {
+
+	@Override
+	public String getLanguage() {
+		// TODO Auto-generated method stub
+		return "TQL";
+	}
+
+	@Override
+	public Object eval(Map<String, Object> parameters) {
+		// TODO create a WCS datasource
+		return null;
+	}
+
+}
---
+++ plugins/org.integratedmodelling.thinklab.core/src/org/integratedmodelling/thinklab/functions/time/TIME.java
@@ -0,0 +1,23 @@
+package org.integratedmodelling.thinklab.functions.time;
+
+import java.util.Map;
+
+import org.integratedmodelling.thinklab.api.knowledge.IExpression;
+import org.integratedmodelling.thinklab.interfaces.annotations.Function;
+
+@Function(id="time", parameterNames= { "start", "end", "duration", "period", "resolution"})
+public class TIME implements IExpression {
+
+	@Override
+	public String getLanguage() {
+		// TODO Auto-generated method stub
+		return "TQL";
+	}
+
+	@Override
+	public Object eval(Map<String, Object> parameters) {
+		// TODO create a WCS datasource
+		return null;
+	}
+
+}
