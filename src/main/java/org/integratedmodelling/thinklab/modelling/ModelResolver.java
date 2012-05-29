510410525815616d76004b90315e85eb28ae420f
Ferdinando Villa <ferdinando.villa@gmail.com>
2012-05-29 09:03:55 +0200

    ff

--- src/main/java/org/integratedmodelling/thinklab/modelling/ModelResolver.java
+++ src/main/java/org/integratedmodelling/thinklab/modelling/ModelResolver.java
@@ -1,10 +1,14 @@
 package org.integratedmodelling.thinklab.modelling;
 
+import org.integratedmodelling.thinklab.annotation.SemanticObject;
 import org.integratedmodelling.thinklab.api.knowledge.ISemanticObject;
 import org.integratedmodelling.thinklab.api.modelling.IContext;
 import org.integratedmodelling.thinklab.api.modelling.IModel;
 import org.integratedmodelling.thinklab.api.modelling.INamespace;
+import org.integratedmodelling.thinklab.api.modelling.IState;
 import org.integratedmodelling.thinklab.modelling.lang.Model;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
 
 /**
  * A resolver for models in a namespace. Will apply heuristics and configured rules to determine
@@ -33,6 +37,54 @@ public class ModelResolver {
 		_namespace = namespace;
 	}
 	
+	class ModelRef {
+		
+		IModel _model = null;
+		int    _index = -1;
+		IState _state = null;
+		String _fname = null;
+		String _observableSig = null;
+		
+		public ModelRef(IModel model) {
+			
+		}
+		
+		/*
+		 * can be compared with a semantic object (observable)
+		 * (non-Javadoc)
+		 * @see java.lang.Object#equals(java.lang.Object)
+		 */
+		@Override
+		public boolean equals(Object arg0) {
+
+			return (arg0 instanceof ModelRef && ((ModelRef)arg0)._observableSig.equals(_observableSig))
+					||
+					(arg0 instanceof SemanticObject<?> && ((SemanticObject<?>)arg0).getSignature().equals(_observableSig));
+		}
+		@Override
+		public int hashCode() {
+			return _observableSig.hashCode();
+		}	
+	}
+	
+	class DependencyEdge extends DefaultEdge {
+		private static final long serialVersionUID = 2366743581134478147L;
+		boolean isMediation = false;
+	}
+	
+	/*
+	 * the "mother" dependency graph, containing reference to resolved or resolvable 
+	 * models. Created at compile() time and used to create actual
+	 * model graphs for each possible solution.
+	 * 
+	 * Circular dependencies are possible but should be resolved on the actual
+	 * model graphs, as each solution may bring in different dependencies. 
+	 * Creating this queries the kbox for unresolved observables.
+	 */
+	DefaultDirectedGraph<ModelRef, DependencyEdge> _modelstruc = null;
+	ModelRef _root = null;
+
+	
 	/**
 	 * Get the best model for the semantic object passed. If the passed object
 	 * is a model, just return it, so we can use it also to "resolve" when
@@ -51,5 +103,14 @@ public class ModelResolver {
 		return null;
 	}
 
+	/**
+	 * Collect models and dependencies from graph, coalescing dependencies for multiple
+	 * observables into a graph with a unique model per node; analyze the resulting graph for cycles
+	 * 
+	 * @return
+	 */
+	public boolean isAcyclic() {
+		return true;
+	}
 	
 }
